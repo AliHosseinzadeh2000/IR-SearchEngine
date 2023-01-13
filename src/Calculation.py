0d3f350310c0164be395961068b8c8a1180c2166
Mehrdad Jafari <asedmehrdad@gmail.com>
2023-01-13 13:36:38 +0330

    Add idf calculation for a list
    Also a bug fix for cosine calculation. cos_vector was tested and it works fine

--- src/Calculation.py
+++ src/Calculation.py
@@ -19,6 +19,13 @@ def calculate_normal_idf(df, total_doc_count):
     return math.log(total_doc_count / df, 10)
 
 
+def calculate_normal_idf_for_all(dfs, total_doc_count):
+    idfs = []
+    for index in range(len(dfs)):
+        idfs.append(calculate_normal_idf(dfs[index], total_doc_count))
+    return idfs
+
+
 def calculate_normal_tfidf(tf, df):
     return math.log(1 + tf, 10) * calculate_normal_idf(df)
 
@@ -32,6 +39,6 @@ def vector_size(vector):
 
 def cos_vector(vector1, vector2):
     dot_result = dot_product(vector1, vector2)
-    vector1_size = vector1_size(vector1)
+    vector1_size = vector_size(vector1)
     vector2_size = vector_size(vector2)
     return dot_result / (vector1_size * vector2_size)
