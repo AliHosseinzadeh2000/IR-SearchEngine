2160ecce1a9da6ac5f6c03aabdf48e2ee16a8ffa
Mehrdad Jafari <asedmehrdad@gmail.com>
2023-01-21 23:54:28 +0330

    Update main program flow

--- src/temp_main.py
+++ src/temp_main.py
@@ -11,8 +11,10 @@ def main():
     while True:
         query = input("Enter your query please:\t").strip().split()
         query = normalizer.normalize_word_list(query)
-        query_vector = calculation.make_vector_from_query(query)  # todo : move this to the 'calculation' module
-        # ranked_documents = calculation.get_ranked_documents(query)
+        # query_vector = calculation.make_vector_from_query(query)  # todo : move this to the 'calculation' module
+        ranked_documents = calculation.get_ranked_documents(query)
+        for entry in ranked_documents.items():
+            print(entry[0], ' : ', entry[1])
 
 
 main()
